ac3d0f2934789d5161e4f08e64254f1d8d59fb36
Christopher Fish <thefishmaster2000@gmail.com>
2020-09-01 05:18:04 -0600

    Added all operations, created basic tester. Needs exception handling

---
+++ Spreadsheet/Tester/Program.cs
@@ -0,0 +1,27 @@
+﻿using FormulaEvaluator;
+using System;
+
+namespace Tester
+{
+    class Program
+    {
+        public static int VarRef(String var)
+        {
+            if (var == "a")
+                return 5;
+            else if (var == "b")
+                return 3;
+            else if (var == "c")
+                return 8;
+            else return 0;
+        }
+
+       
+
+        static void Main(string[] args)
+        {
+            String example = "(1+2+3)-7";
+            Console.WriteLine(Evaluator.Evaluate(example, VarRef));
+        }
+    }
+}
