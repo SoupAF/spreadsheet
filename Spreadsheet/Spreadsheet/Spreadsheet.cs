d15023fd236907c921dde30ca198774c74c3cf85
Christopher Fish <thefishmaster2000@gmail.com>
2020-09-24 22:16:30 -0600

    Implemented all methods other than SetCellContents variants, added test class. Adjusted Cell class format to all use one dependency graph, and changed the Cell hashset to a dictionary

--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -8,24 +8,64 @@ using System.Text;
 
 namespace SS
 {
-    class Spreadsheet : AbstractSpreadsheet
+    public class Spreadsheet : AbstractSpreadsheet
     {
-        //HashSet of all named cells in the spreadsheet. There is infinite empty cells, but only named ones are kept track of here
-        HashSet<string> cells = new HashSet<string>();
+        //Dictionary of all named cells in the spreadsheet. There is infinite empty cells, but only named ones are kept track of here
+        private Dictionary<string, Cell> cells = new Dictionary<string, Cell>();
+
+        //Dependency graph for the spredsheet. Accessable only by this class
+        private DependencyGraph Dependencies = new DependencyGraph();
 
         public override object GetCellContents(string name)
         {
-            throw new NotImplementedException();
+            return cells[name].Value;
         }
 
         public override IEnumerable<string> GetNamesOfAllNonemptyCells()
         {
-            throw new NotImplementedException();
+            return cells.Keys;
         }
 
+
+        /// <summary>
+        /// If name is null or invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, the contents of the named cell becomes number.  The method returns a
+        /// list consisting of name plus the names of all other cells whose value depends, 
+        /// directly or indirectly, on the named cell.
+        /// 
+        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// list {A1, B1, C1} is returned.
+        /// </summary>
         public override IList<string> SetCellContents(string name, double number)
         {
-            throw new NotImplementedException();
+            //Check validity of the name
+            if (!checkValidity(name))
+                throw new InvalidNameException();
+
+            List<string> result = new List<string>();
+
+
+            
+            //If the cell has been previously named and given a value, change it and update dependencies
+            if (cells.ContainsKey(name))
+            {
+                cells[name].Value = number;
+
+                //TODO: UPDATE CELL DEPENDENCIES
+
+            }
+
+            //If the cell has never been used, add it to the cells set, and give it a value
+            else
+            {
+                Cell newCell = new Cell(name, number);
+                cells.Add(name, newCell);
+                //TODO: UPDATE CELL DEPENDENCIES
+            }
+
+
+            return result;
         }
 
         public override IList<string> SetCellContents(string name, string text)
@@ -38,29 +78,75 @@ namespace SS
             throw new NotImplementedException();
         }
 
+
         protected override IEnumerable<string> GetDirectDependents(string name)
         {
-            throw new NotImplementedException();
+            return Dependencies.GetDependents(name);
         }
-    }
 
-    /// <summary>
-    /// Represents a single cell in a spreadsheet
-    /// Each cell has a name, a value, and a DependencyGraph of all its dependents
-    /// </summary>
-    class Cell
-    {
-        private string Name;
-        private Object Value;
-        private DependencyGraph Dependencies;
 
-        public Cell(string name, Object value)
+        /// <summary>
+        /// Private helper method used to check cell names for validity based on the following rules:
+        ///   (1) its first character is an underscore or a letter
+        ///   (2) its remaining characters (if any) are underscores and/or letters and/or digits
+        /// NOTE: This method works on variable names as well, since the naming rules are the same for both
+        /// 
+        /// </summary>
+        /// <param name="name">Input cell name to be tested</param>
+        /// <returns>a boolean value for if the given name is valid or not</returns>
+        private bool checkValidity(string name)
         {
-            Name = name;
-            Value = value;
-            Dependencies = new DependencyGraph();
+            //Split name into characters
+            char[] chars = name.ToCharArray();
+
+
+            //Check first character in name
+            if (!Char.IsLetter(chars[0]) && chars[0] != '_')
+                return false;
+
+            //Check the rest of the characters
+            for (int t = 1; t < chars.Length; t++)
+            {
+                if (!Char.IsDigit(chars[t]) && !Char.IsLetter(chars[0]) && chars[0] != '_')
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
 
+
+        /// <summary>
+        /// Represents a single cell in a spreadsheet
+        /// Each cell has a name, a value, and a DependencyGraph of all its dependents
+        /// Methods are provided to change the value and update dependencies
+        /// </summary>
+        class Cell
+        {
+            //Once set, the name of any given cell can never be changed
+            private string Name;
+
+            
+
+            public Cell(string name, Object value)
+            {
+                Name = name;
+                Value = value;
+                
+            }
+
+
+            /// <summary>
+            /// Getter and setter for the value property of a cell. 
+            /// </summary>
+            public Object Value
+            {
+                get { return Value; }
+                set { Value = value; }
+            }
+
+
+        }
     }
 }
---
+++ Spreadsheet/SpreadsheetTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SS;
+using SpreadsheetUtilities;
+
+namespace SpreadsheetTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestValidityCheck()
+        {
+            AbstractSpreadsheet s1 = new Spreadsheet();
+            
+        }
+    }
+}
